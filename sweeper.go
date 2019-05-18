13f53b4580df3445d5e285d28aa7f453b9a72259
Damien Stamates <thedoubledimensions@gmail.com>
2019-05-18 08:18:11 -0500

    Switched to byte slice and renamed funcs

--- sweeper.go
+++ sweeper.go
@@ -1,10 +1,9 @@
 package sweeper
 
 import (
+	"bytes"
 	"errors"
 	"io"
-	"strings"
-	// "bytes"
 )
 
 const (
@@ -14,18 +13,15 @@ const (
 )
 
 var (
-	ErrBufferFull   = errors.New("sweeper: buffer full")
 	errNegativeRead = errors.New("sweeper: reader returned negative count from Read")
 )
 
 // Sweeper implements buffering for an io.Reader object.
 type Sweeper struct {
-	buf          []byte
-	rd           io.Reader // reader provided by the client
-	r, w         int       // buf read and write positions
-	err          error
-	lastByte     int // last byte read for UnreadByte; -1 means invalid
-	lastRuneSize int // size of last rune read for UnreadRune; -1 means invalid
+	buf  []byte
+	rd   io.Reader // reader provided by the client
+	r, w int       // buf read and write positions
+	err  error
 }
 
 // NewSweeperSize returns a new Sweeper whose buffer has at least the specified
@@ -33,9 +29,9 @@ type Sweeper struct {
 // size, it returns the underlying Sweeper.
 func NewSweeperSize(rd io.Reader, size int) *Sweeper {
 	// Is it already a Reader?
-	b, ok := rd.(*Sweeper)
-	if ok && len(b.buf) >= size {
-		return b
+	s, ok := rd.(*Sweeper)
+	if ok && len(s.buf) >= size {
+		return s
 	}
 	if size < minReadBufferSize {
 		size = minReadBufferSize
@@ -51,50 +47,58 @@ func NewSweeper(rd io.Reader) *Sweeper {
 }
 
 // Size returns the size of the underlying buffer in bytes.
-func (b *Sweeper) Size() int { return len(b.buf) }
+func (s *Sweeper) Size() int { return len(s.buf) }
 
 // Buffered returns the number of bytes that can be read from the current buffer.
-func (b *Sweeper) Buffered() int { return b.w - b.r }
+func (s *Sweeper) Buffered() int { return s.w - s.r }
 
 // Reset discards any buffered data, resets all state, and switches
 // the buffered reader to read from r.
-func (b *Sweeper) Reset(r io.Reader) {
-	b.reset(b.buf, r)
+func (s *Sweeper) Reset(r io.Reader) {
+	s.reset(s.buf, r)
 }
 
-func (b *Sweeper) reset(buf []byte, r io.Reader) {
-	*b = Sweeper{
-		buf:          buf,
-		rd:           r,
-		lastByte:     -1,
-		lastRuneSize: -1,
+func (s *Sweeper) reset(buf []byte, r io.Reader) {
+	*s = Sweeper{
+		buf: buf,
+		rd:  r,
 	}
 }
 
-func (b *Sweeper) readErr() error {
-	err := b.err
-	b.err = nil
+func (s *Sweeper) readErr() error {
+	err := s.err
+	s.err = nil
 	return err
 }
 
-func (b *Sweeper) fillSingleByte() {
-	b.buf = append(b.buf, make([]byte, 1)...)
+// isZero is a helper function to find if a byte slice is all zeroes.
+func (s *Sweeper) isBufZero() bool {
+	for _, v := range s.buf {
+		if v != 0 {
+			return false
+		}
+	}
+	return true
+}
+
+func (s *Sweeper) fill() {
+	s.buf = append(s.buf, make([]byte, 1)...)
 
 	// if the read position is greater than zero then the delimiter was found.
-	if b.r > 0 {
+	if s.r > 0 {
 		// Since the delimiter was found we may reset the buffer back to its
 		// original size to clean up.
-		temp := b.buf[b.r:]
-		b.buf = make([]byte, defaultBufSize)
-		copy(b.buf, temp)
+		temp := s.buf[s.r:]
+		s.buf = make([]byte, defaultBufSize)
+		copy(s.buf, temp)
 
 		// Just set the read and write positions to 0 so then it can scan
 		// from the beginning of the slice when it begins again.
-		b.w = len(temp) - 1
-		b.r = 0
+		s.w = len(temp) - 1
+		s.r = 0
 	}
 
-	if b.w >= len(b.buf) {
+	if s.w >= len(s.buf) {
 		panic("bufio: tried to fill full buffer")
 	}
 
@@ -103,83 +107,69 @@ func (b *Sweeper) fillSingleByte() {
 		// Reads the length of the data that's not part of the already
 		// existing data that I appended earlier. This means that it will
 		// search a total of one byte in this function call.
-		n, err := b.rd.Read(b.buf[b.w:])
+		n, err := s.rd.Read(s.buf[s.w:])
 		if n < 0 {
 			panic(errNegativeRead)
 		}
 
-		b.w += n
+		s.w += n
 		if err != nil {
-			b.err = err
+			s.err = err
 			return
 		}
 		if n > 0 {
 			return
 		}
 	}
-	b.err = io.ErrNoProgress
+	s.err = io.ErrNoProgress
 }
 
-func (b *Sweeper) isZero(s *[]byte) bool {
-	for _, v := range *s {
-		if v != 0 {
-			return false
-		}
-	}
-	return true
-}
-
-// ReadSliceWithString reads until the first occurrence of delim in the input,
-// returning a slice pointing at the bytes in the buffer.
-// The bytes stop being valid at the next read.
+// ReadSliceWithString reads until the first occurrence of the delimiter in the
+// input, returning a slice pointing at the bytes in the buffer.
+// Any bytes that is after the delimiter is saved for the next read.
 // If ReadSliceWithString encounters an error before finding a delimiter,
-// it returns all the data in the buffer and the error itself. Although is the
-// error is for EOF it keeps running until there isn't any data left.
+// it returns all the data in the buffer and the error itself.
+// Although if the error is for EOF it keeps running until there isn't any
+// data left and is just zeroed out.
 // ReadSlice returns err != nil if and only if line does not end in delim.
-func (b *Sweeper) ReadSliceWithString(delim string) (line []byte, err error) {
-	b.fillSingleByte() // Fill the buffer with data
+func (s *Sweeper) ReadSliceWithString(delim []byte) (line []byte, err error) {
+	s.fill() // Fill the buffer with data
 
 	for {
 		// Search buffer.
-		if i := strings.Index(string(b.buf[b.r:]), delim); i >= 0 {
-			line = b.buf[:i+len(delim)]
-			b.r = i + len(delim)
+		if i := bytes.Index(s.buf[s.r:], []byte(delim)); i >= 0 {
+			line = s.buf[:i+len(delim)]
+			s.r = i + len(delim)
 
 			break
 		}
 
 		// Pending error?
-		if b.err != nil && b.err != io.EOF {
-			line = b.buf[b.r:b.w]
-			b.r = b.w
-			err = b.readErr()
+		if s.err != nil && s.err != io.EOF {
+			line = s.buf[s.r:s.w]
+			s.r = s.w
+			err = s.readErr()
 			break
 		}
 
 		// Note: This function does not check the buffered size in comparison
-		// to the length of the buffer, because b.r is always zero and never
+		// to the length of the buffer, because s.r is always zero and never
 		// incremented since we are rescanning all of the buffer all of the time.
 
-		if b.err != io.EOF {
-			b.fillSingleByte()
+		if s.err != io.EOF {
+			s.fill()
 		} else {
-			if b.isZero(&b.buf) {
-				line = b.buf
-				b.r = b.w
-				err = b.err
+			if s.isBufZero() {
+				line = s.buf
+				s.r = s.w
+				err = s.err
 				break
 			}
 
-			b.buf = b.buf[b.r:]
+			s.buf = s.buf[s.r:]
 
-			b.r = 0
+			s.r = 0
 		}
-	}
-
-	// Handle last byte, if any.
-	if i := len(line) - 1; i >= 0 {
-		b.lastByte = int(line[i])
-		b.lastRuneSize = -1
 	}
 
 	return
@@ -191,46 +181,41 @@ func (b *Sweeper) ReadSliceWithString(delim string) (line []byte, err error) {
 // hence n may be less than len(p).
 // To read exactly len(p) bytes, use io.ReadFull(b, p).
 // At EOF, the count will be zero and err will be io.EOF.
-func (b *Sweeper) Read(p []byte) (n int, err error) {
+func (s *Sweeper) Read(p []byte) (n int, err error) {
 	n = len(p)
 	if n == 0 {
-		return 0, b.readErr()
+		return 0, s.readErr()
 	}
-	if b.r == b.w {
-		if b.err != nil {
-			return 0, b.readErr()
+	if s.r == s.w {
+		if s.err != nil {
+			return 0, s.readErr()
 		}
-		if len(p) >= len(b.buf) {
+		if len(p) >= len(s.buf) {
 			// Large read, empty buffer.
 			// Read directly into p to avoid copy.
-			n, b.err = b.rd.Read(p)
+			n, s.err = s.rd.Read(p)
 			if n < 0 {
 				panic(errNegativeRead)
 			}
-			if n > 0 {
-				b.lastByte = int(p[n-1])
-				b.lastRuneSize = -1
-			}
-			return n, b.readErr()
+			return n, s.readErr()
 		}
 		// One read.
-		// Do not use b.fill, which will loop.
-		b.r = 0
-		b.w = 0
-		n, b.err = b.rd.Read(b.buf)
+		// Do not use s.fill, which will loop.
+		s.r = 0
+		s.w = 0
+		n, s.err = s.rd.Read(s.buf)
 		if n < 0 {
 			panic(errNegativeRead)
 		}
 		if n == 0 {
-			return 0, b.readErr()
+			return 0, s.readErr()
 		}
-		b.w += n
+		s.w += n
 	}
 
 	// copy as much as we can
-	n = copy(p, b.buf[b.r:b.w])
-	b.r += n
-	b.lastByte = int(b.buf[b.r-1])
-	b.lastRuneSize = -1
+	n = copy(p, s.buf[s.r:s.w])
+	s.r += n
+
 	return n, nil
 }
